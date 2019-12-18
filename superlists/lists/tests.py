413c1a7966e6a6ba27d471dbf2fdc15803ff1a75
wangjianan <1358314685@qq.com>
2019-12-18 20:59:11 +0800

    lists应用返回简单的html页面

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls.base import resolve
+from django.http import HttpRequest
 from .views import home_page
 
 # Create your tests here.
@@ -9,7 +10,7 @@ from .views import home_page
 class SmokeTest(TestCase):
 
     def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
 
 
 class HomePageTest(TestCase):
@@ -17,6 +18,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_current_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
 
 if __name__ == '__main__':
