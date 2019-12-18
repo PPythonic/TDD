b4c13e64b6a33df44e5ecdf94b055ff49dff981c
wjn <1358314685@qq.com>
2019-12-18 20:37:18 +0800

    第一次单元测试和url地址映射，伪视图函数

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls.base import resolve
+from .views import home_page
 
 # Create your tests here.
 # run command: python manage.py test lists.tests
@@ -8,6 +10,13 @@ class SmokeTest(TestCase):
 
     def test_bad_math(self):
         self.assertEqual(1 + 1, 3)
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
 
 if __name__ == '__main__':
