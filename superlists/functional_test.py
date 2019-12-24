4df2b410bf23cefd17983e0e6db91974075ab4d6
wjn <1358314685@qq.com>
2019-12-24 11:08:51 +0800

    屏蔽csrf_token，建立列表模型，创建迁移文件

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 撕葱听说了一个很酷的在线待办事项应用
         # 他去看了这个应用的首页
@@ -35,16 +40,18 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: 学习数据结构和算法”
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 学习数据结构和算法' for row in rows))
-        self.fail('finish the test!')
-
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他在文本输入框中输入了“趣谈Linux操作系统”
         # 撕葱做事很有规划
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('趣谈Linux操作系统')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，他的清单显示了这两个待办事项
+        self.check_for_row_in_list_table('1:学习数据结构和算法')
+        self.check_for_row_in_list_table('2:趣谈Linux操作系统')
+        self.fail('finish the test!')
+
 
         # 撕葱想知道网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的url
